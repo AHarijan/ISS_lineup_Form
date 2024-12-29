a5059bac74c6bb0cc5093edf43998b2dac678b8f
AHarijan <alakar_harijan@outlook.com>
2024-12-29 20:04:11 +0530

    new updates

--- js/LineupForm.js
+++ js/LineupForm.js
@@ -3,30 +3,28 @@ function lineUpForm(){
     const etaTime = document.getElementById('etatime').value;
     const etbDate = document.getElementById('etbdate').value;
     const etbTime = document.getElementById('etbtime').value;
+    const etcDate = document.getElementById('etcdate').value;
+    const etcTime = document.getElementById('etctime').value;
     
    const eta = etaDate +" "+ etaTime
    const etb = etbDate +" "+ etbTime
-
-    console.log(etaDate)
-    console.log(etaTime) 
-    console.log(etbDate) 
-    console.log(etbTime)  
-    console.log(eta)
-    console.log(etb)
+   const etc = etcDate +" "+ etcTime
 
     if (etb < eta) {
-        alert("ETA cannot be greater that ETB")
+        alert("Vessel cannot berth before Arriving. Please check the Dates and time for ATA and ETB/ATB");
+    return false;
+    }
+    else if (etc < etb) {
+        alert("Vessel cannot sail before Berthing. Please check the Dates and time for ATB and ETC/ATC");
     }
     return false;
 
-
-
 }
 
-// var today = new Date()
-// const year = today.getFullYear();
-// const month = today.getMonth() +1;
-// const day = today.getDate();
+var today = new Date()
+const year = today.getFullYear();
+const month = today.getMonth() +1;
+const day = today.getDate();
 
-// const currDate = `${year}-${month}-${day}`;
-// document.getElementById('currentDate').value=currDate
+const currDate = `${year}-${month}-${day}`;
+document.getElementById('currentDate').value=currDate
